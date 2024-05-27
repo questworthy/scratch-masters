e39b7a43b488ceeba6d74be1db928d60947155a2
commitsovercoffee <commitsovercoffee@gmail.com>
2024-05-27 08:05:32 +0530

    updates toolkits page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	<p class="text-8xl font-extrabold">Scratch Your<br /> Coding Itch !</p>
 	<a href="/toolkits">
 		<button
-			class="rounded-full px-6 py-4 bg-[#333] text-[#fcfaf4] text-4xl font-bold hover:bg-[#e85344] transition-all duration-200 ease-in-out"
+			class="rounded-full px-6 py-4 bg-[#f9a474] text-[#5B2715] text-4xl font-bold drop-shadow-lg hover:scale-125 hover:rotate-3 hover:-translate-y-3 transition-all duration-200 ease-in-out"
 			>Explore Toolkits</button
 		>
 	</a>
--- src/routes/toolkits/+page.svelte
+++ src/routes/toolkits/+page.svelte
@@ -1,2 +1,23 @@
-This is scratch basics page.
+<script>
+	import gender from '$lib/assets/gender.png';
+	import scratch from '$lib/assets/scratch.png';
+	const style =
+		'rounded-2xl px-6 py-4 flex flex-col justify-start drop-shadow-lg hover:scale-110 hover:-translate-y-8 hover:z-10 transition-all duration-200 ease-in-out cursor-pointer';
+</script>
 
+<div class="mx-auto max-w-screen-md grid grid-cols-1 md:grid-cols-2 gap-8 my-16">
+	<div class="{style} bg-[#c3b2e7] text-[#52225E] hover:rotate-6">
+		<img class="m-0" src={gender} alt="" />
+		<p class="mb-0 text-[#52225E] font-bold text-4xl">Gender Equity</p>
+		<p class="font-light text-2xl">
+			Team up for fairness! Let's explore how everyone can shine bright.
+		</p>
+	</div>
+	<div class="{style} bg-[#c9d990] text-[#1C471F] hover:rotate-3">
+		<img class="m-0" src={scratch} alt="" />
+		<p class="mb-0 text-[#1C471F] font-bold text-4xl">Scratch Basics</p>
+		<p class="font-light text-2xl">
+			Coding fun starts here! Learn the basics and become a Scratch master!
+		</p>
+	</div>
+</div>
